36e5d80680b5f20fea6971db771e262221d16eff
Pabitra Dakua <appudq670@gmail.com>
2026-09-19 02:56:56 +0530

    added code

--- task0-runner/main.go
+++ task0-runner/main.go
@@ -2,37 +2,85 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"sync"
 	"time"
 )
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go worker(ctx,1,&wg)
-	go worker(ctx,2,&wg)
-	go worker(ctx,3,&wg)
-
-	time.Sleep(time.Second * 5)
-	fmt.Println("stopping all the workers")
-	cancel()
-	wg.Wait()
-	fmt.Println("all the worker stopped")
+// insert the main data to the pipeline here
+func InsertDataToPipeline(ctx context.Context, data string, inputChan chan<- string) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("error from done channal", ctx.Err())
+			return
+		case inputChan <- data:
+		}
+	}
 }
 
-func worker(ctx context.Context, workerId int, wg *sync.WaitGroup) {
+// push worker
+func pushWorker(ctx context.Context, wg *sync.WaitGroup, stream <-chan string) {
 	defer wg.Done()
 	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println("worker", workerId, "stopped")
+			log.Println("stopping email sub worker", ctx.Err())
+			return;
+		case data,ok := <-stream:
+			if !ok {
+				log.Println("something went wrong in push worker")
+				return;
+			}
+			log.Println(data)
+		}
+	}
+
+}
+
+// email worker
+func emailWorker(parentCtx context.Context, parentWg *sync.WaitGroup, stream <-chan string) {
+	defer parentWg.Done()
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithTimeout(parentCtx,time.Second * 5)
+	defer cancel()
+	for i := 1; i <= 3; i++ {
+		wg.Add(1)
+		go emailSubWorker(ctx, &wg, stream)
+	}
+	wg.Wait()
+
+}
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var wg sync.WaitGroup
+	emailNotificationChannals := make(chan string)
+	pushNotificationChannals := make(chan string)
+	go InsertDataToPipeline(ctx, "email", emailNotificationChannals)
+	go InsertDataToPipeline(ctx, "push", pushNotificationChannals)
+
+	wg.Add(1)
+	go pushWorker(ctx,&wg,pushNotificationChannals)
+	wg.Add(1)
+	go emailWorker(ctx,&wg,emailNotificationChannals)
+
+	wg.Wait()
+}
+
+func emailSubWorker(ctx context.Context, wg *sync.WaitGroup, stream <-chan string) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("stopping email sub worker", ctx.Err())
 			return
-		default:
-			fmt.Println("worker", workerId, "is working..")
-			time.Sleep(time.Second)
+		case data, ok := <-stream:
+			if !ok {
+				log.Println("something went wrong in recving data in email sub worker")
+				return
+			}
+			log.Println(data)
 		}
 	}
 }
